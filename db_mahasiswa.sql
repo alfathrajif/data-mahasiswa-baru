3dcd993eed77c878e4863e08c7ed64fbc19c1b52
Alfath Rajif <31409185+alfathrajif@users.noreply.github.com>
2023-03-05 19:43:42 +0700

    Update SQL

--- db_mahasiswa.sql
+++ db_mahasiswa.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 05, 2023 at 01:35 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
